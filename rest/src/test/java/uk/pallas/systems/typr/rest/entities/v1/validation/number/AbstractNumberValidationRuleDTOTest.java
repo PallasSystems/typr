946f647257efdb22f8697e0fce242f3f95af1daf
Stephen Crocker <stephen.crocker@pallas.uk>
2023-06-07 14:49:55 +0100

    Added more tests

--- rest/src/test/java/uk/pallas/systems/typr/rest/entities/v1/validation/number/AbstractNumberValidationRuleDTOTest.java
+++ rest/src/test/java/uk/pallas/systems/typr/rest/entities/v1/validation/number/AbstractNumberValidationRuleDTOTest.java
@@ -2,6 +2,7 @@ package uk.pallas.systems.typr.rest.entities.v1.validation.number;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import uk.pallas.systems.typr.entities.v1.validation.ValidationRuleConstants;
 import uk.pallas.systems.typr.rest.entities.v1.validation.StringValidationRuleDTO;
 import uk.pallas.systems.typr.entities.v1.validation.StringValidationRule;
 import uk.pallas.systems.typr.entities.v1.validation.number.DoubleValidationRule;
@@ -63,7 +64,58 @@ public abstract class AbstractNumberValidationRuleDTOTest
   @Test
   void testIsValid() {
     final T basic = this.generateTestInstance();
-
     Assertions.assertTrue(basic.isValid(this.getValidNumber()));
+  }
+
+  @Test
+  void testIsValidWithNullMax() {
+    final T basic = this.generateTestInstance();
+    // configure the maximum number to null to confirm everything becomes true
+    basic.setMaximumValue(null);
+    Assertions.assertTrue(basic.isValid(this.getValidNumber()));
+    Assertions.assertTrue(basic.isValid(Long.MAX_VALUE));
+
+    // We expect this value to be lower than our test minimum and so should fail
+    Assertions.assertFalse(basic.isValid(Long.MIN_VALUE));
+  }
+
+  @Test
+  void testIsValidWithNullMin() {
+    final T basic = this.generateTestInstance();
+    // configure the maximum number to null to confirm everything becomes true
+    basic.setMinimumValue(null);
+    Assertions.assertTrue(basic.isValid(this.getValidNumber()));
+    Assertions.assertFalse(basic.isValid(Long.MAX_VALUE));
+
+    // We expect this value to be lower than our test maximum and so should pass
+    Assertions.assertTrue(basic.isValid(Long.MIN_VALUE));
+  }
+
+  @Test
+  void testSetUnit() {
+    final T basic = this.generateTestInstance();
+
+    final String alternate = "Example";
+    basic.setUnit(alternate);
+    Assertions.assertEquals(alternate, basic.getUnit());
+  }
+
+  @Test
+  void testSetUnitWithInvalid() {
+    final T basic = this.generateTestInstance();
+
+    final String alternate = "Example";
+    basic.setUnit(alternate);
+    Assertions.assertEquals(alternate, basic.getUnit());
+
+    basic.setUnit(null);
+    Assertions.assertEquals(ValidationRuleConstants.NO_UNITS, basic.getUnit());
+
+    basic.setUnit(alternate);
+    Assertions.assertEquals(alternate, basic.getUnit());
+
+    basic.setUnit(" ");
+    Assertions.assertEquals(ValidationRuleConstants.NO_UNITS, basic.getUnit());
+
   }
 }
