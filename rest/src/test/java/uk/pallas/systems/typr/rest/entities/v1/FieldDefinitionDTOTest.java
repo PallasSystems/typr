bd0f72e5257f470b43b5064bde334fd6529611d0
Stephen Crocker <stephen.crocker@pallas.uk>
2023-06-18 20:36:31 +0100

    Updated the tests, to ensure they are easier to read

--- rest/src/test/java/uk/pallas/systems/typr/rest/entities/v1/FieldDefinitionDTOTest.java
+++ rest/src/test/java/uk/pallas/systems/typr/rest/entities/v1/FieldDefinitionDTOTest.java
@@ -1,29 +1,51 @@
 package uk.pallas.systems.typr.rest.entities.v1;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import uk.pallas.systems.typr.entities.v1.FieldDefinition;
 import uk.pallas.systems.typr.entities.v1.validation.ValidationRule;
 import uk.pallas.systems.typr.rest.entities.v1.validation.number.LongValidationRuleDTO;
 
 class FieldDefinitionDTOTest {
-  
-  @Test
-  void testConstructor() {
-    final String fieldName = "FieldDefinitionDTOTest - testConstructor";
-    final String acronym = "FDDT-tc";
-    final String description = "FieldDefinitionDTOTest-testConstructor";
 
-    final long max = 1234;
-    final long min = 987;
-    final String ruleDescription = "LongValidationRuleDTO-testConstructor";
-    final String unitName = "Knot";
-    final ValidationRule rule = new LongValidationRuleDTO(max,min,description, unitName);
+  /**
+   * This will generate a test object within a number validation rule, that has a range of 100-10000.
+   * @return a valid and completly formed Field Definition.
+   */
+  private FieldDefinition generateTestObject() {
+    final String fieldName = "FieldDefinitionDomainTest-generateTestObject";
+    final String acronym = "FDDT-gto";
+    final String description = "FieldDefinitionDomainTest-generateTestObject";
+
+    final long max = 10000;
+    final long min = 100;
+    final String ruleDescription = "LongValidationRuleDomain-generateTestObject";
+    final String unitName = "Degree Angle";
+    final ValidationRule rule = new LongValidationRuleDTO(max,min,ruleDescription, unitName);
     final Set<ValidationRule> rules = new HashSet<>();
     rules.add(rule);
 
-    final FieldDefinitionDTO basic = new FieldDefinitionDTO(acronym, new HashSet<>(), description,
+    return new FieldDefinitionDTO(acronym, new HashSet<>(), description, fieldName, rules);
+  }
+
+  @Test
+  void testConstructor() {
+    final String fieldName = "FieldDefinitionDomainTest - testConstructor";
+    final String acronym = "FDDT-tc";
+    final String description = "FieldDefinitionDomainTest-testConstructor";
+
+    final long max = 1234;
+    final long min = 987;
+    final String ruleDescription = "LongValidationRuleDomain-testConstructor";
+    final String unitName = "Knot";
+    final ValidationRule rule = new LongValidationRuleDTO(max,min,ruleDescription, unitName);
+    final Set<ValidationRule> rules = new HashSet<>();
+    rules.add(rule);
+
+    final FieldDefinition basic = new FieldDefinitionDTO(acronym, new HashSet<>(), description,
       fieldName, rules);
 
     Assertions.assertEquals(fieldName, basic.getName());
@@ -36,24 +58,64 @@ class FieldDefinitionDTOTest {
 
   @Test
   void testCopyConstructor() {
-    final String fieldName = "FieldDefinitionDTOTest - testCopyConstructor";
-    final String acronym = "FDDT-tc";
-    final String description = "FieldDefinitionDTOTest-testCopyConstructor";
-
-    final long max = 9876;
-    final long min = 432;
-    final String ruleDescription = "LongValidationRuleDTO-testCopyConstructor";
-    final String unitName = "Degree Angle";
-    final ValidationRule rule = new LongValidationRuleDTO(max,min,description, unitName);
-    final Set<ValidationRule> rules = new HashSet<>();
-    rules.add(rule);
-
-    final FieldDefinitionDTO basic = new FieldDefinitionDTO(acronym, new HashSet<>(), description,
-      fieldName, rules);
-
-    final FieldDefinitionDTO copy = new FieldDefinitionDTO(basic);
+    final FieldDefinition basic = this.generateTestObject();
+    final FieldDefinition copy = new FieldDefinitionDTO(basic);
 
     Assertions.assertEquals(basic, copy);
+    Assertions.assertEquals(copy, copy);
+    Assertions.assertEquals(basic.hashCode(), copy.hashCode());
   }
 
+  @Test
+  void testCopyConstructorWithInvalid() {
+    final FieldDefinition basic = new FieldDefinitionDTO();
+    final FieldDefinition nullCopy = new FieldDefinitionDTO(null);
+
+    Assertions.assertEquals(basic, nullCopy);
+    Assertions.assertEquals(basic.hashCode(), nullCopy.hashCode());
+  }
+
+  @Test
+  void testEqualsWithInvalid() {
+    final FieldDefinition basic = this.generateTestObject();
+
+    Assertions.assertNotEquals(null, basic);
+    Assertions.assertNotEquals(basic, new FieldDefinitionDTO());
+    Assertions.assertNotEquals(basic.hashCode(), new FieldDefinitionDTO().hashCode());
+  }
+
+  @Test
+  void testIsValid() {
+    final FieldDefinition basic = this.generateTestObject();
+
+    // Supply a value which is within our target range, this should pass and return the toString
+    // of the rule we created above.
+    final Collection<String> passed = basic.getRulesPassed(555);
+    Assertions.assertNotNull(passed);
+    Assertions.assertFalse(passed.isEmpty());
+    Assertions.assertEquals(basic.getRules().size(), passed.size());
+  }
+
+  @Test
+  void testGetRulesPassed() {
+    final FieldDefinition basic = this.generateTestObject();
+
+    // Supply a value which is within our target range, this should pass and return the toString
+    // of the rule we created above.
+    final Collection<String> passed = basic.getRulesPassed(555);
+    Assertions.assertNotNull(passed);
+    Assertions.assertFalse(passed.isEmpty());
+    Assertions.assertEquals(basic.getRules().size(), passed.size());
+  }
+
+  @Test
+  void testGetRulesPassedWithInvalid() {
+    final FieldDefinition basic = this.generateTestObject();
+
+    // Supply a value which is within our target range, this should pass and return the toString
+    // of the rule we created above.
+    final Collection<String> passed = basic.getRulesPassed(null);
+    Assertions.assertNotNull(passed);
+    Assertions.assertTrue(passed.isEmpty());
+  }
 }
